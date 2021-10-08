63495c87d6be08a1588f825a573f9b05047847bb
Lypzis <victorvp2018@protonmail.com>
2021-10-08 17:28:04 -0300

    update

---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- now it is available to wrap any other component :D -->
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  margin: 2rem auto;
+  max-width: 40rem;
+}
+</style>
---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,91 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'duckduckgo',
+          title: 'Duck Duck Go',
+          description: 'Learn to google.',
+          link: 'https://start.duckduckgo.com'
+        }
+      ]
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    }
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource,
+      deleteResource: this.removeResource
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title,
+        description,
+        link
+      };
+
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
+    },
+    removeResource(resId) {
+      // THIS WILL NOT CHANGE THE CURRENT 'PROVIDED' ARRAY
+      // AS IT IS CREATING A NEW ONE, WRONG APPROACH
+      // this.storedResources = this.storedResources.filter(
+      //   res => res.id !== resId
+      // );
+
+      // this here is correct, as manipulates the original array
+      const resIndex = this.storedResources.findIndex(res => res.id === resId);
+
+      this.storedResources.splice(resIndex, 1);
+    }
+  }
+};
+</script>
